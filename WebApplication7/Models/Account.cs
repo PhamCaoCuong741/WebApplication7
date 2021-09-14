68ef7673d0354d42e48489a6b31951a46d9c7035
CUONG PHAM CAO <1821050741@student.humg.edu.vn>
2021-09-14 09:54:10 +0700

    Add project files.

---
+++ WebApplication7/Models/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BaiNgay7hang9.Models
+{
+    public class Account
+    {
+        [Key]
+        public string Usename { get; set; }
+        public string Password { get; set; }
+
+    }
+}
---
+++ WebApplication7/Models/LTQLBbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BaiNgay7hang9.Models
+{
+    public partial class LTQLDbContext : DbContext
+    {
+        public LTQLDbContext() : base("name=LTQLDbContext")
+        {
+        }
+        public virtual DbSet<Account> Accounts { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Account>()
+           .Property(e => e.Usename)
+           .IsUnicode(false);
+            modelBuilder.Entity<Account>().Property(e => e.Password)
+            .IsUnicode(false);
+        }
+    }
+}
