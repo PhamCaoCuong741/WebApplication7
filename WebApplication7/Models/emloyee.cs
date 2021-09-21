1159a536091c351bd2a63e4074036a495fad61b2
CUONG PHAM CAO <1821050741@student.humg.edu.vn>
2021-09-21 08:54:31 +0700

    a

---
+++ WebApplication7/Models/emloyee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication7.Models
+{
+    public class emloyee
+    {
+        
+        public string EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public DateTime EmployeeBirth { get; set; }
+    }
+}
